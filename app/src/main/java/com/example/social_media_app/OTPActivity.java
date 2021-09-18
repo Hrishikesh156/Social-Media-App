54a8ebba989d09ecab778d4a188b3726c191c450
Hrishikesh kumbhar <hrishikumbhar156@gmail.com>
2021-09-18 13:32:57 +0530

    removed action bar

--- app/src/main/java/com/example/social_media_app/OTPActivity.java
+++ app/src/main/java/com/example/social_media_app/OTPActivity.java
@@ -10,5 +10,6 @@ public class OTPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otpactivity);
+        getSupportActionBar().hide();
     }
 }
