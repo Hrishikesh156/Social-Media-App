df813102b4bcdaf8aa84fb4f11c0f96d99084da3
Mukul Kolpe <mukulkolpe45@gmail.com>
2021-10-06 02:35:26 +0530

    Email field gets updated on OTP Activity

--- app/src/main/java/com/example/social_media_app/EmailActivity.java
+++ app/src/main/java/com/example/social_media_app/EmailActivity.java
@@ -2,14 +2,31 @@ package com.example.social_media_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.social_media_app.databinding.ActivityEmailBinding;
 
 public class EmailActivity extends AppCompatActivity {
+
+    ActivityEmailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_email);
+        binding = ActivityEmailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(EmailActivity.this, OTPActivity.class);
+                intent.putExtra("EmailID", binding.mailBox.getText().toString());
+                startActivity(intent);
+            }
+
+        });
     }
 }
